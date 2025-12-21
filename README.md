dc0156d554430cd0894a44789126034b92c2b22a
Chosen1 PR <chosen1pr@redd.it>
2025-12-20 23:01:57 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,34 @@
+## Features
+
+This app allows moderators to edit their AutoModerator config from the Reddit app for iOS or Android. It also creates a more convenient shortcut on desktop for quick editing, but the page in Mod Tools is still the best place to be able to see more of your Automod code at once.
+
+This app also respects mod permissions, so mods who are not explicitly allowed to edit Automod according to their mod permissions will not be able to edit Automod using this app either.
+
+### General Usage Instructions
+
+1. Install the app on your subreddit using the "Add to community" button.
+2. Visit your subreddit using the Reddit mobile app.
+3. Tap the three dots (...) in the upper right hand corner of the screen.
+4. Select Automod.
+5. Make your changes.
+6. Provide a reason for editing. This reason must be under 200 characters long.
+7. Submit your changes.
+
+That's it! Due to developer platform limitations, this app is not able to provide detailed error messages if your Automod code is incorrect. Please ensure that your YAML code is free of any syntax errors and your edit reason is under 200 characters before submitting.
+
+---
+
+## Changelog
+
+### [1.0.0] Initial version (2025-12-20)
+
+#### Features
+
+- Edit your AutoModerator config directly from the Reddit mobile app via a menu item on your subreddit.
+- Accessible via mobile or desktop.
+- Easily see which mod made changes to Automod in the mod log.
+- Allow only authorized mods to make changes to Automod.
+
+#### Bug Fixes
+
+None yet (initial version). Please send a private message to the developer (u/Chosen1PR) to report bugs.
---
+++ src/server/index.ts
@@ -0,0 +1,170 @@
+import express from "express";
+import {
+  createServer,
+  context,
+  getServerPort,
+  reddit
+} from "@devvit/web/server";
+
+const app = express();
+
+// Middleware for JSON body parsing
+app.use(express.json());
+// Middleware for URL-encoded body parsing
+app.use(express.urlencoded({ extended: true }));
+// Middleware for plain text body parsing
+app.use(express.text());
+
+const router = express.Router();
+
+// Menu item for editing automod
+router.post("/internal/menu/edit-automod", async (_req, res) => {
+  // First, check if mod has permission to edit Automod.
+  const mod = (await reddit.getUserById(context.userId!))!;
+  let hasConfigPermission = false;
+  let hasWikiPermission = false;
+  let hasAllPermissions = false;
+  const permissions = await mod.getModPermissionsForSubreddit(context.subredditName);
+  const permissionErrorMessage = "You do not have permission to edit Automod.";
+  if (permissions) {
+    for (let i = 0; i < permissions.length; i++) {
+      if (permissions[i] == "all") {
+        hasAllPermissions = true;
+        break;
+      }
+      else if (permissions[i] == "config") {
+        hasConfigPermission = true;
+        if (hasWikiPermission) break;
+      }
+      else if (permissions[i] == "wiki") {
+        hasWikiPermission = true;
+        if (hasConfigPermission) break;
+      }
+    }
+    const hasIndividualPermissions = (hasConfigPermission && hasWikiPermission);
+    if (!(hasAllPermissions || hasIndividualPermissions)) {
+      res.json({
+        showToast: permissionErrorMessage
+      });
+      return;
+    }
+  }
+  else {
+    res.json({
+      showToast: permissionErrorMessage
+    });
+    return;
+  }
+  // If we got here, we have the necessary permissions.
+  // Get the current Automod config.
+  const wikiPage = await reddit.getWikiPage(context.subredditName, 'config/automoderator');
+  let automodConfig = "";
+  try {
+    automodConfig = wikiPage.content ?? "";
+  }
+  catch (error) {
+    automodConfig = "";
+  }
+  res.json({
+    showForm: {
+      name: 'editAutomodForm',
+      form: {
+        title: 'Configure Automod',
+        fields: [
+          {
+            type: 'paragraph',
+            name: 'automodConfig',
+            label: 'Automod Config',
+            lineHeight: 15,
+            defaultValue: automodConfig,
+            required: true
+          },
+          {
+            type: 'string',
+            name: 'editReason',
+            label: 'Edit Reason',
+            helpText: '200 character limit',
+            required: true
+          },
+        ],
+      },
+      acceptLabel: 'Submit',
+      cancelLabel: 'Cancel',
+      data: {
+        automodConfig: automodConfig
+      }
+    }
+  });
+});
+
+// Form submission handler for Automod
+router.post("/internal/forms/edit-automod-submit", async (req, res) => {
+  const automodConfig = req.body.automodConfig as string ?? "";
+  var editReason = req.body.editReason as string ?? "";
+  if (editReason.length > 200) {
+    res.json({
+      showToast: "Error: Edit reason is too long."
+    });
+    return;
+  }
+  editReason += ` | Edited by u/${context.username!}`;
+  const defaultErrorMessage = "Error: Please check for mistakes in your config and try again.";
+  try {
+    const wikiPage = await reddit.updateWikiPage({
+      subredditName: context.subredditName,
+      page: 'config/automoderator',
+      content: automodConfig,
+      reason: editReason,
+    });
+    if (wikiPage) {
+      res.json({
+        showToast: "Automod updated successfully."
+      });
+      return;
+    }
+    else {
+      res.json({
+        showToast: defaultErrorMessage
+      });
+    }
+  }
+  catch (err) {
+    const error = err as Error;
+    if (error.message.includes("RESTRICTED_PAGE")) {
+      try {
+        const wikiPage = await reddit.createWikiPage({
+          subredditName: context.subredditName,
+          page: 'config/automoderator',
+          content: automodConfig,
+          reason: editReason,
+        });
+        if (wikiPage) {
+          res.json({
+            showToast: "Automod created successfully."
+          });
+          return;
+        }
+        else {
+          res.json({
+            showToast: defaultErrorMessage
+          });
+        }
+      }
+      catch {
+        res.json({
+          showToast: defaultErrorMessage
+        });
+      }
+    }
+    else {
+      res.json({
+        showToast: defaultErrorMessage
+      });
+    }
+  }
+});
+app.use(router);
+
+const server = createServer(app);
+server.on("error", (err) => console.error(`server error: ${err.stack}`));
+server.listen(getServerPort());
